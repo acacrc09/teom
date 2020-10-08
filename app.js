4107ed2a3b0ffbff4e9f6ac85bf386c5693dd0b1
Joab Gonzalez <joabgonzalez@gmail.com>
2020-10-08 16:39:57 -0300

    modificando estructura para servicios

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var adminRouter = require('./routes/admin');
+var indexRouter = require('./routes/views/index');
+var adminRouter = require('./routes/views/admin');
 
 var app = express();
 
@@ -23,19 +23,19 @@ app.use('/', indexRouter);
 app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
---
+++ services/indicators.js
@@ -0,0 +1,15 @@
+const mock = require('../');
+
+class IndicatorsSrevices {
+  constructor() {}
+
+  get() {}
+
+  create() {}
+
+  update() {}
+
+  delete() {}
+}
+
+module.exports = IndicatorsSrevices;
