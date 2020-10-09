068d65fea632b267f9cc8815210545918f9e00cf
Omar Vergara <omar.vergara@tsoftglobal.com>
2020-10-09 11:53:55 -0300

    Finalizado mongodb

--- services/indicators.js
+++ services/indicators.js
@@ -1,6 +1,5 @@
 const indicatorsMock = require('../utils/moks/indicators');
 const MongoLib = require("../lib/mongo");
-const _ = require('lodash');
 
 class IndicatorsSrevices {
   constructor() {
@@ -13,50 +12,34 @@ class IndicatorsSrevices {
     const indicators = await this.mongoDB.getAll(this.collection, query);
 
     return indicators || [];
-    //return Promise.resolve({ indicators: indicatorsMock });
   }
 
   async get({ id }) {
     const indicator = await this.mongoDB.get(this.collection, id);
     return indicator || {};
-    //return Promise.resolve({ indicator: _.find(indicatorsMock, { id }) });
   }
 
   async create({ body }) {
-    console.log('llego');
-    
     const createIndicatorId = await this.mongoDB.create(this.collection, body);
-
     return createIndicatorId;
-
-    //return Promise.resolve({ newIndicator: body });
   }
 
   async update({ id, body }) {
-
     const updateIndicatorId = await this.mongoDB.update(
       this.collection,
       id,
       body
     );
     return updateIndicatorId;
-    
-    //return Promise.resolve({
-    //  newIndicator: body,
-    //  oldIndicator: _.find(indicatorsMock, { id }),
-    //});
+
   }
 
   async delete({ id }) {
-
     const deletedIndicatorId = await this.mongoDB.delete(
       this.collection,
       id
     );
-
     return deletedIndicatorId;
-
-    //return Promise.resolve({ oldIndicator: _.find(indicatorsMock, { id }) });
   }
 }
 
