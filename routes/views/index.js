7c2cf450123358f4b5c826a6df7e8e70eb3f2f3c
Victor Gonzalez <v.gonzalez.o@gmail.com>
2020-10-08 23:12:01 -0300

    Sitio 0.1

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+
 var router = express.Router();
 
+
 router.get('/', function(req, res, next) {
-    res.render('index');
+    let rawdata = fs.readFileSync('./public/dummy.json');
+    let items = JSON.parse(rawdata);
+
+    res.render('index', { items: items });
 });
 
 module.exports = router;
