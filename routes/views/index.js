2c80d16fd424cc8d0f8042e1e679b79874788d13
Victor Gonzalez <v.gonzalez.o@gmail.com>
2020-10-16 21:07:21 -0300

    Front con Form

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,14 +1,25 @@
-var express = require('express');
-var fs = require('fs');
+const express = require('express');
+//var fs = require('fs');
+const IndicatorsServices = require('../../services/indicators')
+const indicatorsServices = new IndicatorsServices();
+const router = express.Router();
 
-var router = express.Router();
+sorting = function(a, b) {
+    if (a.rate < b.rate) return -1
+    if (a.rate > b.rate) return 1
 
+    return 0
+};
 
-router.get('/', function(req, res, next) {
-    let rawdata = fs.readFileSync('./public/dummy.json');
-    let items = JSON.parse(rawdata);
-
-    res.render('index', { items: items });
+router.all('/', async function(req, res) {
+    //let rawdata = fs.readFileSync('./public/dummy.json');
+    //let items = JSON.parse(rawdata);
+    let items = await indicatorsServices.getAll(req.body);
+    console.log(items);
+    res.render('index', {
+        items: items.filter((item) => item.rate > 0).sort(sorting),
+        req: req.body
+    });
 });
 
 module.exports = router;
