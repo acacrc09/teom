e956256492db0502b2833bab7d4ed1495e9d0248
Victor Gonzalez <v.gonzalez.o@gmail.com>
2020-10-19 16:56:21 -0300

    Agregando pruebas

---
+++ nightwatch.conf.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "src_folders": ["test/nightwatch"],
+
+    "webdriver": {
+        "start_process": true,
+        "server_path": "node_modules/chromedriver/lib/chromedriver/chromedriver.exe",
+        "port": 9515
+    },
+
+    "test_settings": {
+        "default": {
+            "desiredCapabilities": {
+                "browserName": "chrome",
+                "javascriptEnabled": true,
+                "chromeOptions": {
+                    "args": ["--headless", "--no-sandbox", "--disable-gpu"]
+                }
+            }
+        }
+    }
+}
